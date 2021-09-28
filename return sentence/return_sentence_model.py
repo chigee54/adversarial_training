ab377d95c0ec0dec3213f6244958ab28ad9ffbce
chigee54 <86865792+chigee54@users.noreply.github.com>
2021-09-28 15:54:16 +0800

    Update return_sentence_model.py

--- return sentence/return_sentence_model.py
+++ return sentence/return_sentence_model.py
@@ -1,17 +1,16 @@
 #! -*- coding: utf-8 -*-
 # return the token embeddings to the sentences utilizing network model
-
 import numpy as np
+from bert4keras.backend import keras
 from keras.models import Model
+from keras.layers import Input, Dense
 from bert4keras.optimizers import Adam
 from bert4keras.tokenizers import Tokenizer
-from keras.layers import Input, Dense
 from sklearn.metrics import accuracy_score
-from bert4keras.backend import keras, search_layer, K
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-data_path = 'datasets/lcqmc/lcqmc.train.data'
-embedding_matrix = np.load('datasets/lcqmc/token_embedding/embedding_matrix.npy', allow_pickle=True)
-tokenizer = Tokenizer(token_dict='../../chinese_L-12_H-768_A-12/vocab.txt', do_lower_case=True)
+tokenizer = Tokenizer(token_dict='C:/Users/admin/Desktop/generatot/chinese_L-12_H-768_A-12/vocab.txt', do_lower_case=True)
 
 
 def load_data(filename):
@@ -23,16 +22,15 @@ def load_data(filename):
     return D
 
 
-def convert_input_data(data_path):
-    token_list, embedding_list, sentence_len = [], [], []
+def convert_data(data_path, test_flag=False):
+    token_list, sentence_len = [], []
     train_data = load_data(data_path)
     for text in train_data:
         token_ids, segment_ids = tokenizer.encode(text[0], text[1], maxlen=128)
-        for id in token_ids:
-            embedding_list.append(embedding_matrix[id])
         token_list.extend(token_ids)
-        sentence_len.append(len(token_ids))
-    return np.array(embedding_list), np.array(token_list), sentence_len
+        if test_flag is True:
+            sentence_len.append(len(token_ids))
+    return np.array(token_list), sentence_len
 
 
 def return_sentences(token_ids, length):
@@ -46,8 +44,8 @@ def return_sentences(token_ids, length):
 
 
 inputs = Input(shape=(768,))
-x = Dense(100, activation='relu')(inputs)
-x = Dense(100, activation='relu')(x)
+x = Dense(100, activation='gelu')(inputs)
+x = Dense(100, activation='gelu')(x)
 output = Dense(21128, activation='softmax')(x)
 model = Model(inputs, output)
 
@@ -58,11 +56,12 @@ model.compile(
 )
 
 
-def evaluate(valid_x, valid_y):
-    y_pred = model.predict(valid_x).argmax(axis=1)
-    acc = accuracy_score(valid_y, y_pred)
-    pre_result = return_sentences(y_pred, sentences_length)
-    print(pre_result)
+def evaluate(x, y, sentences_length=None):
+    y_pred = model.predict(x).argmax(axis=1)
+    acc = accuracy_score(y, y_pred)
+    if sentences_length is not None:
+        pre_result = return_sentences(y_pred, sentences_length)
+        print(pre_result[:10])
     return acc
 
 
@@ -73,11 +72,11 @@ class Evaluator(keras.callbacks.Callback):
         self.best_val_acc = 0.
 
     def on_epoch_end(self, epoch, logs=None):
-        val_acc = evaluate(valid_x, valid_y)
+        val_acc = evaluate(valid_x[:2000], valid_y[:2000])
         if val_acc > self.best_val_acc:
             self.best_val_acc = val_acc
-            # model.save_weights('best_model.weights')
-        test_acc = evaluate(test_x, test_y)
+            model.save_weights('weight/best_model.weights')
+        test_acc = evaluate(test_x[:2000], test_y[:2000], test_len[:100])
         print(
             u'val_acc: %.5f, best_val_acc: %.5f, test_acc: %.5f\n' %
             (val_acc, self.best_val_acc, test_acc)
@@ -85,15 +84,20 @@ class Evaluator(keras.callbacks.Callback):
 
 
 if __name__ == '__main__':
-    train_x, train_y, sentences_length = convert_input_data(data_path)
-    valid_x, valid_y, test_x, test_y = train_x, train_y, train_x, train_y
-    evaluator = Evaluator()
+    train_x = np.load('train_embedding.npy', allow_pickle=True)
+    valid_x = np.load('valid_embedding.npy', allow_pickle=True)
+    test_x = np.load('delta_test_embedding.npy', allow_pickle=True)
 
+    train_y, _ = convert_data('../examples/datasets/lcqm-data/lcqmc.train.data')
+    valid_y, valid_len = convert_data('../examples/datasets/lcqm-data/lcqmc.dev.data')
+    test_y, test_len = convert_data('../examples/datasets/lcqm-data/lcqmc.test.data', True)
+    evaluator = Evaluator()
+    model.load_weights('weight/best_model.weights')
     print('Training---------')
     model.fit(
         train_x,
         train_y,
-        epochs=10,
-        batch_size=10,
+        epochs=3,
+        batch_size=42,
         callbacks=[evaluator]
     )
